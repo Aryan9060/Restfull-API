7142cd5c724021f96ed9b92f4d4bac5d55ee5cea
Aryan9060 <Aryanofficial9060@gmail.com>
2026-06-15 14:23:01 +0530

    Add ApiError and ApiResponse utility classes; update dependencies

---
+++ src/common/utils/api-error.js
@@ -0,0 +1,50 @@
+class ApiError extends Error{
+
+    constructor ( statusCode, message){
+        super(message);
+        this.statusCode = statusCode;
+        this.isOperational = true;
+        Error.captureStackTrace(this, this.constructor);
+    
+    }
+
+    static badRequest(message){
+        return new ApiError(400, message);
+    }
+
+    static unauthorized(message){
+        return new ApiError(401, message);
+    }
+
+    static forbidden(message){
+        return new ApiError(403, message);
+    }
+
+    static notFound(message){
+        return new ApiError(404, message);
+    }
+
+    static conflict(message){
+        return new ApiError(409, message);
+    }
+
+    static tooManyRequests(message){
+        return new ApiError(429, message);
+    }
+
+    static internalServerError(message){
+        return new ApiError(500, message);
+    }
+
+    static serviceUnavailable(message){
+        return new ApiError(503, message);
+    }
+
+    static unprocessableEntity(message) {
+        return new ApiError(422, message);
+    }
+    
+
+}
+
+export default ApiError;
