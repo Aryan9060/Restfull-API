b8f996e4dda5e7322304735e2ab762a007df1097
Aryan9060 <Aryanofficial9060@gmail.com>
2026-06-15 14:04:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import app from "./src/app.js";
+import connectDB from "./src/common/config/db.js";
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`);
+    })
+}
+
+start().catch((err)=>{
+    console.error('Faild to start server', err);
+    process.exit(1);
+})
